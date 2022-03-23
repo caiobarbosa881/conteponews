1ba26b9ddc8c1d16d0ea1c243752557658f8e328
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-03-23 17:45:07 -0300

    responsive navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+import { MdFingerprint } from 'react-icons/md';
+import { useState } from 'react';
+import { FaTimes, FaBars} from 'react-icons/fa';
+
+function NavBar() {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960 ) {
+          setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', 
+    showButton);
+
+  return (
+    <>
+        <div className='navbar'>
+            <div className='navbar-container container'>
+                <Link to='/' className="navbar-logo">
+                    <MdFingerprint className='navbar-icon'/>
+                    LAVISH
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links'>
+                            Página Inicial
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links'>
+                            Serviços
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links'>
+                            Produto
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links'>
+                            Contato
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export const homeObjOne = {
+    lightBg: false,
+    lighText: true,
+    lightTextDesc: true,
+    topLine: 'Acesso exclusivo',
+    headline: 'transações ilimitadas com taxa zero',
+    description: 'Tenha Acesso para acessar nosso cartão de diamante exclusivo que vai te permitir mandar transações ilimitadas sem qualquer taxa',
+    buttonLabel: 'Vamos Começar',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'credit Card'
+}
