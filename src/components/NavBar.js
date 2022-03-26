f8bf13be271aec0d5c279eade7f3504e4c9683a5
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-03-26 17:02:07 -0300

    navbar container transition

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,24 +6,17 @@ import { FaTimes, FaBars} from 'react-icons/fa';
 import { BsNewspaper } from 'react-icons/bs';
 
 function NavBar() {
+
     const [click, setClick] = useState(false);
-    
+
     const handleClick = () => setClick(!click);
-
-
-    const scrollingElement = (document.scrollingElement || document.body); scrollingElement.scrollTop = scrollingElement.scrollHeight;
-
-    const scrollToBottom = (id) => {
-        const element = document.getElementById(id);
-        element.scrollTop = element.scrollHeight;
-    }
 
   return (
     <>
         <div className='navbar'>
             <div className='navbar-container container'>
                 <Link to='/' className="navbar-logo">
-                    <BsNewspaper id='logo' className='navbar-icon'/>
+                    <BsNewspaper className="navbar-icon"/>
                     ContepoNews
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
@@ -47,7 +40,7 @@ function NavBar() {
                             Produtos
                         </Link>
                     </li>
-                    <li className='nav-item' onClick={scrollToBottom}>
+                    <li className='nav-item'>
                         <Link to='/'  className='nav-links'>
                             Contatos
                         </Link>
